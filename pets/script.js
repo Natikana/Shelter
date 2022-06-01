f94485d8e7dc54047d02d4d5561c955db9944dfe
Natallia Mashkouskaya <95764339+Natikana@users.noreply.github.com>
2022-06-01 23:15:25 +0200

    feat:add burger parts

--- pets/script.js
+++ pets/script.js
@@ -1,15 +1,13 @@
 const burger = document.querySelector('#burger');
-const popup = document.querySelector('#popup');
-const body = document.body;
+const menu = document.querySelector('.menu');
+const darkShadow = document.querySelector('.dark_shadow');
 
 burger.addEventListener('click', hambHandler);
 
 function hambHandler (event) {
-    event.preventDefault();
-    popup.classList.toggle('open');
+    event.preventDefault(); 
+    menu.classList.toggle('active');
     burger.classList.toggle('active');
-    body.classList.toggle('noscroll');
-
-   
+    darkShadow.classList.toggle('active');
 }
 
